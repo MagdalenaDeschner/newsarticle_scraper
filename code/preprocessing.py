4d90f811d45919110ac3f2175b5923a3f845ae82
Magdalena <mdeschner@hotmail.de>
2018-10-20 22:44:00 +0200

    implemented option to tokenize text to sentences --> commented out

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -1,3 +1,4 @@
+from nltk.tokenize import sent_tokenize
 import pandas as pd
 import pickle as pkl 
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -10,6 +11,12 @@ fileObject.close()
 
 # check size of data
 print(articles.shape)
+
+# split news articles into sentences
+# articles["body_sent"] = articles.body.apply(sent_tokenize)
+# print(articles.iloc[9,:])
+
+# get distribution of category labels
 print(articles.category.value_counts())
 
 # split data into body and label
